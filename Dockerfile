24391b6fd087ae9002441ead41356bc9c4caf0ea
Kennytian <kt.tian@gmail.com>
2021-12-02 12:19:10 +0800

    feat: Update ts, Dockerfile and yarn.lock file

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM node:lts-alpine
 WORKDIR /app
 COPY . ./
 ENV PORT 3000
+ENV TZ Asia/Shanghai
+RUN apk add tzdata && cp /usr/share/zoneinfo/${TZ} /etc/localtime \
+    && echo ${TZ} > /etc/timezone \
+    && apk del tzdata
 EXPOSE $PORT
 RUN yarn && yarn build
 CMD ["yarn", "start:prod"]
--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-	console.log(`request at ${new Date().toString()}`)
-  	res.send("<h1>Hi Kenny!!!</h1>");
+  const date = new Date().toString();
+  console.log(`request at ${date}`);
+  res.send(`<h1>Hi Kenny at ${date}</h1>`);
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
